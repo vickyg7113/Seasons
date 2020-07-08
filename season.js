73a5e4299454ff0f8f5db734339791eb1870bb90
vickyg7113 <63911764+vickyg7113@users.noreply.github.com>
2020-07-08 22:17:01 +0530

    Update season.js

--- season.js
+++ season.js
@@ -1,40 +1,51 @@
-function spring()
+function season()
 {
- var spring=document.getElementById("spring");
- spring.setAttribute("src","spring.gif");
- spring.setAttribute("width","100%");
- spring.setAttribute("height","100%");
- spring.setAttribute("border-radius","30px");
-}
-function summer()
-{
- var summer=document.getElementById("summer");
- summer.setAttribute("src","summer.gif");
- summer.setAttribute("width","100%");
- summer.setAttribute("height","100%");
- summer.setAttribute("border-radius","30px");
-}
-function monsoon()
-{
- var monsoon=document.getElementById("monsoon");
- monsoon.setAttribute("src","monsoon.gif");
- monsoon.setAttribute("width","100%");
- monsoon.setAttribute("height","100%");
- monsoon.setAttribute("border-radius","30px");
-}
-function autumn()
-{
- var autumn=document.getElementById("autumn");
- autumn.setAttribute("src","autumn.gif");
- autumn.setAttribute("width","100%");
- autumn.setAttribute("height","100%");
- autumn.setAttribute("border-radius","30px");
-}
-function winter()
-{
- var winter=document.getElementById("winter");
- winter.setAttribute("src","winter.gif");
- winter.setAttribute("width","100%");
- winter.setAttribute("height","100%");
- winter.setAttribute("border-radius","30px");
+  var value=document.getElementById("season");
+  var result=value.options[value.selectedIndex].value;
+  
+  if(result=="spring")
+   {
+    var spring=document.getElementById("image");
+    spring.setAttribute("src","spring.gif");
+    spring.setAttribute("alt","Spring");
+    spring.setAttribute("width","100%");
+    spring.setAttribute("height","100%");
+    spring.setAttribute("border-radius","30px");
+     }
+  else if(result=="summer")
+  {
+    var summer=document.getElementById("image");
+    summer.setAttribute("src","summer.gif");
+    summer.setAttribute("alt","Summer");
+    summer.setAttribute("width","100%");
+    summer.setAttribute("height","100%");
+    summer.setAttribute("border-radius","30px");
+  }
+  else if(result=="monsoon")
+  {
+    var monsoon=document.getElementById("image");
+    monsoon.setAttribute("src","monsoon.gif");
+    monsoon.setAttribute("alt","Monsoon");
+    monsoon.setAttribute("width","100%");
+    monsoon.setAttribute("height","100%");
+    monsoon.setAttribute("border-radius","30px");
+   }
+  else if(result=="autumn")
+  {
+    var autumn=document.getElementById("image");
+    autumn.setAttribute("src","autumn.gif");
+    autumn.setAttribute("alt","Autumn");
+    autumn.setAttribute("width","100%");
+    autumn.setAttribute("height","100%");
+    autumn.setAttribute("border-radius","30px");
+  }
+  else
+  {
+    var winter=document.getElementById("image");
+    winter.setAttribute("src","winter.gif");
+    winter.setAttribute("alt","Winter");
+    winter.setAttribute("width","100%");
+    winter.setAttribute("height","100%");
+    winter.setAttribute("border-radius","30px");
+  }
 }
