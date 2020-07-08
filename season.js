1344fef81ceeff9ff60a26dab171c08bfe032f4d
vickyg7113 <63911764+vickyg7113@users.noreply.github.com>
2020-07-08 20:24:43 +0530

    Update season.js

--- season.js
+++ season.js
@@ -1,35 +1,40 @@
 function spring()
 {
  var spring=document.getElementById("spring");
- spring.setAttribute("src","vamshi/spring.gif");
+ spring.setAttribute("src","spring.gif");
  spring.setAttribute("width","100%");
  spring.setAttribute("height","100%");
+ spring.setAttribute("border-radius","30px");
 }
 function summer()
 {
  var summer=document.getElementById("summer");
- summer.setAttribute("src","vamshi/summer.gif");
+ summer.setAttribute("src","summer.gif");
  summer.setAttribute("width","100%");
  summer.setAttribute("height","100%");
+ summer.setAttribute("border-radius","30px");
 }
 function monsoon()
 {
  var monsoon=document.getElementById("monsoon");
- monsoon.setAttribute("src","vamshi/monsoon.gif");
+ monsoon.setAttribute("src","monsoon.gif");
  monsoon.setAttribute("width","100%");
  monsoon.setAttribute("height","100%");
+ monsoon.setAttribute("border-radius","30px");
 }
 function autumn()
 {
  var autumn=document.getElementById("autumn");
- autumn.setAttribute("src","vamshi/autumn.gif");
+ autumn.setAttribute("src","autumn.gif");
  autumn.setAttribute("width","100%");
  autumn.setAttribute("height","100%");
+ autumn.setAttribute("border-radius","30px");
 }
 function winter()
 {
  var winter=document.getElementById("winter");
- winter.setAttribute("src","vamshi/winter.gif");
+ winter.setAttribute("src","winter.gif");
  winter.setAttribute("width","100%");
  winter.setAttribute("height","100%");
+ winter.setAttribute("border-radius","30px");
 }
