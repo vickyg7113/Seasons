00bc4630c6efd51cf71b18f0520704d2b2cb413c
vickyg7113 <63911764+vickyg7113@users.noreply.github.com>
2020-07-08 20:19:13 +0530

    Update season.js

--- season.js
+++ season.js
@@ -1,1 +1,35 @@
-
+function spring()
+{
+ var spring=document.getElementById("spring");
+ spring.setAttribute("src","vamshi/spring.gif");
+ spring.setAttribute("width","100%");
+ spring.setAttribute("height","100%");
+}
+function summer()
+{
+ var summer=document.getElementById("summer");
+ summer.setAttribute("src","vamshi/summer.gif");
+ summer.setAttribute("width","100%");
+ summer.setAttribute("height","100%");
+}
+function monsoon()
+{
+ var monsoon=document.getElementById("monsoon");
+ monsoon.setAttribute("src","vamshi/monsoon.gif");
+ monsoon.setAttribute("width","100%");
+ monsoon.setAttribute("height","100%");
+}
+function autumn()
+{
+ var autumn=document.getElementById("autumn");
+ autumn.setAttribute("src","vamshi/autumn.gif");
+ autumn.setAttribute("width","100%");
+ autumn.setAttribute("height","100%");
+}
+function winter()
+{
+ var winter=document.getElementById("winter");
+ winter.setAttribute("src","vamshi/winter.gif");
+ winter.setAttribute("width","100%");
+ winter.setAttribute("height","100%");
+}
